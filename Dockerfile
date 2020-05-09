cbad78adc1f7755207143ff3bdc39935803dd4ed
HouzuoGuo <guohouzuo@gmail.com>
2020-05-09 08:01:04 +0000

    Put busybox, curl, wget into Dockerfile for easier diagnosis; print incoming request in application log

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 # https://github.com/HouzuoGuo/k8s-hello-world
 FROM ubuntu
 
+RUN apt update && apt install -y busybox curl wget
+
 COPY my-app /my-app
 
 ENV PORT 20000
--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,8 +11,9 @@ func main() {
 	log.Printf("Going to listen on port %s", os.Getenv("PORT"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("Serving URL \"%s\" to visitor \"%s:%s\"", req.Host, req.URL.Port(), req.URL)
 		hostName, _ := os.Hostname()
-		_, _ = w.Write([]byte("Hello from " + hostName))
+		_, _ = fmt.Fprintf(w, "Hello from host name %s, your host appears to be %s, your requested URL is:%s", hostName, req.Host, req.URL)
 	})
 
 	if err := http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil); err != nil {
